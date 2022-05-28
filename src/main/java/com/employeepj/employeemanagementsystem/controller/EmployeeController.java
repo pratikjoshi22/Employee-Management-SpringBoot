bcd0a0aac58541f182591c30065d3d7e3e022a96
Pratik <joshipratik300@gmail.com>
2022-05-28 11:06:33 +0530

    implemented update employees and delete employees

--- src/main/java/com/employeepj/employeemanagementsystem/controller/EmployeeController.java
+++ src/main/java/com/employeepj/employeemanagementsystem/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -36,4 +37,22 @@ public class EmployeeController {
         employeeService.saveEmployee(employee);
         return "redirect:/";
     }
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable( value = "id") long id, Model model) {
+
+        // get employee from the service
+        Employee employee = employeeService.getEmployeeById(id);
+
+        // set employee as a model attribute to pre-populate the form
+        model.addAttribute("employee", employee);
+        return "update_employee";
+    }
+    @GetMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable(value = "id") long id) {
+
+        // call delete employee method
+        this.employeeService.deleteEmployeeById(id);
+        return "redirect:/";
+    }
+
 }
